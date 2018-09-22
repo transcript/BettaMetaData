297b15136d2d441e6de5a01703f93c95d4e1fcfa
Sam Westreich <swestreich@dnanexus.com>
2018-09-22 11:38:53 -0400

    ENA parser in scripts folder

---
+++ scripts/ENA_parser.py
@@ -0,0 +1,64 @@
+#!/env/python2.7
+
+# basic_parser.py
+# Created 9/20/18 by Sam Westreich, github.com/transcript
+# Function - read through a set of downloaded EBI metadata and give info about the entries in each column.
+
+import sys, re
+
+# help
+if "-h" in sys.argv:
+	print ("USAGE: python ENA_parser.py $input_summary_file [-F]")
+	print ("\t-F flag creates an output file.")
+	sys.exit()
+
+try:
+	infile = open(sys.argv[1], "r")
+except IndexError:
+	sys.exit("Specify the downloaded metadata file as ARGV1.")
+
+line_counter = 0
+dic_list = []
+
+for line in infile:
+	line_counter += 1
+	# header row
+	if line_counter == 1:
+		dic_names = line.strip().split("\t")
+		dic_list = [dict() for item in line.strip().split('\t')]
+	# past header row
+	else:
+		line = line.strip()
+		split = re.split(r'\t', line)
+		for i in range(0, len(split)-1):
+			datum=split[i]
+			position = i
+			if datum == "":
+				datum = "!blank"
+			if datum.isspace() == True:
+				datum = "!blank"
+#			print position, datum
+			try:
+				dic_list[position][datum] += 1
+			except KeyError:
+				dic_list[position][datum] = 1
+				continue
+
+print ("Analyzed file " + sys.argv[1])
+print ("Number of fields: " + str(len(dic_list)))
+
+infile.close()
+
+# outfile stuff
+if "-F" in sys.argv:
+	outfile = open(sys.argv[1].split(".txt")[0] + "_summary.txt", "w")
+	for dictionary in dic_names:
+		exclude = ["accession", "secondary_sample_accession"]
+		if dictionary not in exclude:
+			outfile.write(dictionary + "\n")
+			for k, v in sorted(dic_list[dic_names.index(dictionary)].items(), key=lambda kv: -kv[1])[:100]:
+				outfile.write(k + "\t" + str(v) + "\n")
+			outfile.write("\n")
+
+print ("Excluded fields in summary outfiles: " + str(exclude))
+outfile.close()
